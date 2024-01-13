6142872492e7fc79a8c51881f09800d6fc8732d5
Joey <62597260+ruanzhengyang@users.noreply.github.com>
2024-01-13 15:08:08 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,12 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          小阳的ChatGPT-公益版
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          觉得好用？<a href="https://pay.rzy.zone/" target="_blank" rel="noopener noreferrer">
+            <span className={styles["sidebar-sub-title-gradient"]}>欢迎捐赠</span>
+          </a>
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
